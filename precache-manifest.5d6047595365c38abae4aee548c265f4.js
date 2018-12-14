d5162a5aeb2efc38bc1b98bb0062627e43105c15
thejoulethief <anupamsanthosh@gmail.com>
2018-12-14 11:48:06 +0530

    Updates

---
+++ precache-manifest.5d6047595365c38abae4aee548c265f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a01f63ff33800a73bfcfbfcefdad9d8f",
+    "url": "/static/media/base_c.a01f63ff.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1b585354135d4d442818",
+    "url": "/static/js/main.1b585354.chunk.js"
+  },
+  {
+    "revision": "b671852d0c4b74c90e0f",
+    "url": "/static/js/1.b671852d.chunk.js"
+  },
+  {
+    "revision": "1b585354135d4d442818",
+    "url": "/static/css/main.324706ef.chunk.css"
+  },
+  {
+    "revision": "b671852d0c4b74c90e0f",
+    "url": "/static/css/1.59ded275.chunk.css"
+  },
+  {
+    "revision": "b1882c15bf5c7948dcfdf6f7e7091a63",
+    "url": "/index.html"
+  }
+];
